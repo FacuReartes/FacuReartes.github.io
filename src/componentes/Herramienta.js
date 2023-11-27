5b839db4fc37619d37341d1f304a4a155c10731c
FacuReartes <facureartes9@gmail.com>
2023-11-27 16:52:35 -0300

    agregado skills

--- src/componentes/Herramienta.js
+++ src/componentes/Herramienta.js
@@ -3,9 +3,13 @@ import Icon from './Icon'
 import '../estilos/Herramienta.css'
 
 function Herramienta(props) {
+
   return (
     <div className='herramienta__container'>
-      <Icon icon={props.ig} css='herramienta__icon'/>
+      {props.ig ?
+       <Icon icon={props.ig} css='herramienta__icon'/> 
+       : 
+       <img src={props.svg} alt='mySvgImage' className='herramienta__icon'/>}
     </div>
   )
 }
--- src/componentes/Skills.js
+++ src/componentes/Skills.js
@@ -2,25 +2,30 @@ import React, { forwardRef } from 'react'
 import '../estilos/Skills.css'
 import SubTitulo from './SubTitulo'
 import Herramienta from './Herramienta'
-import { faReact, faPython, faJsSquare, faHtml5, faCss3Alt, faGitAlt } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faPython, faGitAlt } from '@fortawesome/free-brands-svg-icons';
 import { useTranslation } from 'react-i18next';
+import node from '../imagenes/bxl-nodejs.svg'
+import spring from '../imagenes/bxl-spring-boot.svg'
+import tailwind from '../imagenes/bxl-tailwind-css.svg'
+import typescript from '../imagenes/bxl-typescript.svg'
 
 const Skills = forwardRef((props, ref) => {
 
   const [t, i18n] = useTranslation("global");
 
   const mySkills = [
-    { id:1, icon:faReact },
-    { id:2, icon:faJsSquare },
-    { id:3, icon:faPython },
-    { id:4, icon:faHtml5 },
-    { id:5, icon:faCss3Alt },
-    { id:6, icon:faGitAlt}
+    { id:1, icon:faReact},
+    { id:2, svg:typescript},
+    { id:3, svg:tailwind},
+    { id:4, svg:node},
+    { id:5, icon:faGitAlt},
+    { id:6, icon:faPython},
+    { id:7, svg:spring }
   ];
 
   const SkillComponent = mySkills.map((i) => (
     <li>
-      <Herramienta key={i.id} ig={i.icon}/>
+      <Herramienta key={i.id} ig={i.icon} svg={i.svg}/>
     </li>
   ));
 
