0e51b19b1870ebae6d161dbfd237657e91f5517c
FacuReartes <facureartes9@gmail.com>
2023-12-15 14:23:39 -0300

    agregado proyecto despilfarro

--- src/componentes/Proyectos.js
+++ src/componentes/Proyectos.js
@@ -11,6 +11,7 @@ const Proyectos = forwardRef((props, ref) => {
   const myProjects = [
     { id:1, titulo:t("proyectos.p1-title"), p:t("proyectos.p1-text"), link: 'https://noise-chill.vercel.app/'},
     { id:4, titulo:t("proyectos.p4-title"), p:t("proyectos.p4-text"), link:'https://user-authenticator-topaz.vercel.app/home'},
+    { id:6, titulo:t("proyectos.p6-title"), p:t("proyectos.p6-text"), link:'https://despil.vercel.app/' },
     { id:2, titulo:t("proyectos.p2-title"), p:t("proyectos.p2-text"), link: 'https://shoe-store-swart.vercel.app/'},
     { id:3, titulo:t("proyectos.p3-title"), p:t("proyectos.p3-text"), link: 'https://facureartes.github.io/connect-four/'},
     { id:5, titulo:t("proyectos.p5-title"), p:t("proyectos.p5-text"), link:'https://facureartes.github.io/portfolio/'}
